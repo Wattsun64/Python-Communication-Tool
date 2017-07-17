a97f16c5ef1937051572e9395a18ef0dadb9c05e
Edward Cochran <wattsun64@0587340351.wireless.umich.net>
2017-07-17 09:37:12 -0400

    Removed chmod commands as it is not required to run main.py file.

--- local_run.sh
+++ local_run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-chmod u+x main.py       # Changes permission to allow main.py to be executed
 python3 main.py         # Runs main.py file with Python3
-chmod -x main.py        # Removes file execution permissions
