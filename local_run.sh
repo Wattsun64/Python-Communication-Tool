1ac2cc14ae8ceaf94d049e535d865f3023b1ef35
Edward Cochran <wattsun64@0587340351.wireless.umich.net>
2017-07-17 08:58:27 -0400

    Renamed run.sh to local_run.sh as the file is meant to be used for testing. Will create another bash file for installing updated Python version and required packages.

---
+++ local_run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+chmod u+x main.py
+python3 main.py
+chmod -x main.py
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-<<<<<<< HEAD
-
-chmod u+x main.py
-=======
->>>>>>> f4954cfb8d878fe0ab49653e4b44686f458d571f
-python3 main.py
